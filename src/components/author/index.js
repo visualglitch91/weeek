3fd666e47a898cde8c60c154b939953e433fc142
@ericaeumesma <visualglitch91@gmail.com>
2020-02-26 15:21:29 -0300

    Hard reload the app when the author info is double clicked

--- src/components/author/index.js
+++ src/components/author/index.js
@@ -4,15 +4,22 @@ import { heart } from "ionicons/icons";
 
 function Author({ className }) {
   return (
-    <IonText
-      color="medium"
-      style={{ display: "block", fontFamily: "Pacifico", fontSize: 14 }}
-      className={["ion-text-center", "ion-margin-top", className]
-        .filter(Boolean)
-        .join(" ")}
-    >
-      made with <IonIcon icon={heart} style={{ color: "#f5aeae" }} /> by erica
-    </IonText>
+    <div onDoubleClick={() => window.location.reload(true)}>
+      <IonText
+        color="medium"
+        style={{
+          display: "block",
+          fontFamily: "Pacifico",
+          fontSize: 14,
+          userSelect: "none"
+        }}
+        className={["ion-text-center", "ion-margin-top", className]
+          .filter(Boolean)
+          .join(" ")}
+      >
+        made with <IonIcon icon={heart} style={{ color: "#f5aeae" }} /> by erica
+      </IonText>
+    </div>
   );
 }
 
